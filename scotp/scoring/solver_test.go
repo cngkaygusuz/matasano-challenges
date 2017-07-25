fbc51919ceafbbfa2a7e495054f4ec55de8e45b9
Cengiz Kaygusuz <cngkaygusuz@gmail.com>
2017-07-25 16:36:07 +0300

    Comments

--- scotp/scoring/solver_test.go
+++ scotp/scoring/solver_test.go
@@ -7,6 +7,7 @@ import (
 	"log"
 )
 
+// Matasano 3
 func Test_Solve_1(t *testing.T) {
 	input, err := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 	require.Nil(t, err)
@@ -18,4 +19,8 @@ func Test_Solve_1(t *testing.T) {
 		log.Printf("key: %#x", key)
 		log.Printf("plaintext: '%s'", plaintext)
 	}
+
+	// Prints the following:
+	//
+	// Cooking MC's like a pound of bacon
 }
