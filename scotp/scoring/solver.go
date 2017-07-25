e0898f453f543e4a961e364ffe315b642d28de3f
Cengiz Kaygusuz <cngkaygusuz@gmail.com>
2017-07-25 16:34:05 +0300

    Add scoring scotp solver

---
+++ scotp/scoring/solver.go
@@ -0,0 +1,46 @@
+package scoring
+
+import (
+	"github.com/cngkaygusuz/matasano-challenges/common"
+)
+
+func Solve(ciphertext []byte) ([]byte, byte) {
+	candidate := make([]byte, len(ciphertext))
+
+	var best_guess []byte = make([]byte, len(ciphertext))
+	var best_guess_score int = -1
+	var best_guess_key byte
+
+	var i byte
+	var score int
+	for i = 0; i < 0xff; i++ {
+		copy(candidate, ciphertext)
+		score = 0
+
+		for j := 0; j < len(candidate); j++ {
+			candidate[j] = candidate[j] ^ i
+			score += score_byte(candidate[j])
+		}
+
+		if score > best_guess_score {
+			best_guess_score = score
+			copy(best_guess, candidate)
+			best_guess_key = i
+		}
+
+	}
+
+	return best_guess, best_guess_key
+}
+
+func score_byte(b byte) int {
+	if !common.ReadableByte(b) { // case b is not readable
+		return -100000
+	} else if 65 <= b && b <= 90 { // case b is a capital alphabet character
+		return 100
+	} else if 97 <= b && b <= 122 { // case b is a lowercase alphabet character
+		return 100
+	} else { // case b is a readable character but not an alphabet character
+		return 10
+	}
+}
