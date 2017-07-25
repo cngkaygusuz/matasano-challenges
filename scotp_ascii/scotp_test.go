58cd4b47436652c1fc239e9a805af5eab88fd79a
Cengiz Kaygusuz <cngkaygusuz@gmail.com>
2017-07-25 15:57:31 +0300

    Add output note

--- scotp_ascii/scotp_test.go
+++ scotp_ascii/scotp_test.go
@@ -7,6 +7,7 @@ import (
 	"log"
 )
 
+// Matasano 3
 func Test_Solve_1(t *testing.T) {
 	input, err := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 	require.Nil(t, err)
@@ -18,4 +19,12 @@ func Test_Solve_1(t *testing.T) {
 		log.Printf("key: %#x", key)
 		log.Printf("plaintext: '%s'", plaintext)
 	}
+
+	// Running this test prints the following:
+	//
+	// 2017/07/25 15:48:33 key: 0x47
+	// 2017/07/25 15:48:33 plaintext: '\pptvqx?R\8l?svtz?~?opjq{?py?}~|pq'
+	//
+	// I speculate that this idea might have worked if the input set was bigger, but for smaller one, it seems it is
+	// possible that we might get an all-readable ASCII output that is not the original plaintext.
 }
